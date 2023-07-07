3ea1dfeba4acc6e8ff747180c9a91b70786d5ec2
Ravi515K <koliravi95@gmail.com>
2023-07-07 11:30:13 +0530

    ravi add code to mains .html

--- backend/Routers/taskRouter.js
+++ backend/Routers/taskRouter.js
@@ -27,9 +27,11 @@ taskRouter.get("/",async(req,res)=>{
 
 taskRouter.delete("/delete/:id",authenticate, async(req,res)=>{
     const ID=req.params.id;
-
+    
     try {
+       
         let taskDelete= await taskModel.findOneAndDelete({_id:ID})
+        console.log(taskDelete)
         res.send("task has been Deleted")
     } catch (err) {
         res.send({"err":err,"msg":"task not deleted"})
@@ -39,8 +41,9 @@ taskRouter.delete("/delete/:id",authenticate, async(req,res)=>{
 taskRouter.patch("/update/:id",authenticate, async(req,res)=>{
    // console.log("reqqq",req)
     const ID=req.params.id;
+   
     const payload=req.body
-    console.log("pay",ID)
+  
     try {
         let taskUpdated=await taskModel.findOneAndUpdate({_id:ID},payload)
         res.send(taskUpdated)
